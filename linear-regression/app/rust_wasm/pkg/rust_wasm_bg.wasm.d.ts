d7a9ca28ae59d587d9e14efbc4a0c53fc1570ebb
claytonmcast <clayton.cassar.h72604@mcast.edu.mt>
2025-04-10 09:28:25 +0200

    Included pkg folder

---
+++ linear-regression/app/rust_wasm/pkg/rust_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const fetch_and_train: (a: any, b: any) => any;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_4: WebAssembly.Table;
+export const __wbindgen_export_5: WebAssembly.Table;
+export const closure24_externref_shim: (a: number, b: number, c: any) => void;
+export const closure42_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
