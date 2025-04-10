ed82447cc02c6f3373d2f9e6c88d01c182236b63
claytonmcast <clayton.cassar.h72604@mcast.edu.mt>
2025-04-10 09:29:22 +0200

    Included pkg folder

---
+++ neural-network/app/rust_wasm/pkg/rust_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const fetch_and_train: (a: number, b: number, c: number, d: number, e: number) => any;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const closure52_externref_shim: (a: number, b: number, c: any) => void;
+export const closure112_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
